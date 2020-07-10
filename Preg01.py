22b84c21092473d28d925c65be1da2e05f8387f5
Anthony Talavera <anthony082017@gmail.com>
2020-07-09 21:09:07 -0500

    Listo

--- Preg01.py
+++ Preg01.py
@@ -1,3 +1,14 @@
 #Autor: Anthony Talavera Carranza
 
+def esPrimo(numero):
+    if(numero%2==0):
+        print("El número "+str(numero)+" es primo")
+    else:
+        print("El número "+str(numero)+" no es primo")
+
+
+numero=int(input("Ingrese el número para ver si es primo o no ☺: "))
+
+esPrimo(numero)
+
     
